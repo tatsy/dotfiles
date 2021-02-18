a83dfc04c9f95f330272b50a3086a03a1540054d
Tatsuya Yatagawa <tatsy.mail@gmail.com>
2021-02-19 01:56:47 +0900

    Add plane fish script for peco instead of oh-my-fish plugin.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,10 +6,25 @@ set -x PATH $PATH $GOPATH
 alias vim=/usr/local/bin/nvim
 
 # Peco
+function peco_select_history
+  history|peco|read foo
+  if [ $foo ]
+    commandline $foo
+  else
+    commandline ''
+  end
+end
+
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
 end
 
 # GHQ
 set -g GHQ_SELECTOR peco
+
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+status is-interactive && eval /Users/tatsuya/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
 
