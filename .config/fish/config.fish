62e2d7f17dbcc0500e253ac275f946ae690bf7ab
Tatsuya Yatagawa <tatsy.mail@gmail.com>
2024-07-24 17:52:08 +0900

    Minor update.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 # Path
-set -x GOPATH $HOME
-set -x PATH $PATH $GOPATH
+set -x GOROOT /usr/local/go
+set -x GOPATH $HOME/go
+set -x PATH $PATH $GOPATH/bin
 
 # Alias
 alias vim=/usr/local/bin/nvim
@@ -9,6 +10,8 @@ alias vim=/usr/local/bin/nvim
 set -g theme_nerd_fonts yes
 set -g theme_title_display_path yes
 set -g theme_title_use_abbreviated_path no
+set -g theme_project_dir_length 0
+set -g fish_prompt_pwd_dir_length 0
 
 # Peco
 function peco_select_history
@@ -27,6 +30,10 @@ end
 # GHQ
 set -g GHQ_SELECTOR peco
 
+#
+if status is-interactive
+  set PATH /opt/homebrew/bin $PATH
+end
 
 # conda initialize
 status is-interactive && eval /Users/tatsuya/miniconda3/bin/conda "shell.fish" "hook" $argv | source
