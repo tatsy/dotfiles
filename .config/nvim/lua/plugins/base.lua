0fe27998b33a57846fc4ab56c19b75d49f8cc737
Tatsuya Yatagawa <tatsy.mail@gmail.com>
2026-09-03 22:04:45 +0900

    Replace airline with bufferline

--- .config/nvim/lua/plugins/base.lua
+++ .config/nvim/lua/plugins/base.lua
@@ -9,39 +9,19 @@ return {
             vim.cmd([[colorscheme tender]])
         end,
     },
-    -- fancy status line
+    -- status line
     {
         "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+    -- buffer line
+    {
+        "akinsho/bufferline.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
     },
     -- helpful for editing
     { "tpope/vim-endwise" },
     { "Townk/vim-autoclose" },
-    -- airline
-    {
-        "vim-airline/vim-airline-themes",
-        lazy = false,
-    },
-    {
-        "vim-airline/vim-airline",
-        lazy = false,
-        config = function()
-            vim.g.airline_theme = "tender"
-            vim.g.airline_symbols_ascii = 0
-            vim.g.airline_powerline_fonts = 1
-            vim.g.airline_section_z = vim.fn["airline#section#create"]({ "windowswap", "%3p%%", "linenr", ":%3v" })
-            vim.g["airline#extensions#virtualenv#enabled"] = 0
-            vim.g["airline#extensions#hunks#enabled"] = 0
-
-            vim.g["airline#extensions#tabline#enabled"] = 1
-            vim.g["airline#extensions#tabline#show_buffers"] = 1
-            vim.g["airline#extensions#tabline#buffer_idx_mode"] = 1
-            vim.g["airline#extensions#tabline#formatter"] = "unique_tail"
-
-            vim.api.nvim_set_keymap("n", "<C-p>", "<Plug>AirlineSelectPrevTab", {})
-            vim.api.nvim_set_keymap("n", "<C-n>", "<Plug>AirlineSelectNextTab", {})
-        end,
-    },
     -- rainbow parenthesis
     {
         "luochen1990/rainbow",
